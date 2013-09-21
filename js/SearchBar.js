a04c80c8a2d813b6fc8cdc813d99fea394363391
DELOFFRE Cyril <deloffre.c@gmail.com>
2013-09-21 13:41:04 +0200

    UPDATE Search Feature
    Major update

--- js/SearchBar.js
+++ js/SearchBar.js
@@ -6,37 +6,70 @@
 
 	Author : Deloffre Cyril
 	Website : http://neospiro.fr
+	Version 0.2
+
+	Updates : 
+	 + Search feature
 */
 var SearchBar = {
-	initialize : function(searchInput, contentFilter){
+	initialize : function(searchInput, contentFilter)
+	{
 		this.contentFilter = contentFilter;
-		this.content = $(contentFilter);
+		this.searchInput = searchInput;
+		//Init callbacks
 		$(searchInput).change(this.cbSearch())
 			.keyup(this.cbSearch());
+
+		//Implementing new "contains" filter, case insensitive
+		jQuery.expr[':'].Contains = function(a, i, m) {
+		  return jQuery(a).text().toUpperCase()
+		      .indexOf(m[3].toUpperCase()) >= 0;
+		};
+		return this;
 	},
-	find : function(searchRequest){
-		var words = searchRequest.split(" ");
-		$(this.content).css("display","none");
-		for(aWord in words){
-			$(this.content).each(function (aWord) {
-    			if($(this).text().toUpperCase().search(aWord.toUpperCase())!==-1){
-    				$(this).css("display","block");
-    			}
-			},[ words[aWord] ]);
-		}	
+	find : function(searchRequest)
+	{
+		this.content = $(this.contentFilter);
+		if(searchRequest.trim() === "")
+		{
+			$(this.content)
+				.removeClass('SearchBar_find')
+				.removeClass('SearchBar_notfind');
+			return null;
+		}
+
+		var CSSrequest = 
+			":Contains('" + 
+			searchRequest
+				//.replace("'", "\\'") //Not needed Oo
+				.trim()
+				.split(" ")
+				.join(
+					"'),:Contains('"	) + 
+			"')";
+		console.log(CSSrequest);
+		$(this.content.filter(CSSrequest))
+			.addClass('SearchBar_find')
+			.removeClass('SearchBar_notfind');
+		$(this.content.filter( ":not(" + CSSrequest + ")" ))
+			.addClass('SearchBar_notfind')
+			.removeClass('SearchBar_find');
+		return this.content.filter(CSSrequest);
 	},
 	updateContent: function(contentFilter){
-		if(typeof(contentFilter)=="undefined")
-			contentFilter = this.contentFilter
-		else
+		if(typeof(contentFilter)!="undefined")
 			this.contentFilter = contentFilter;
-		this.content = $(contentFilter);
+
+		return this.contentFilter;
 	},
+
+	
 	//callBack
 	cbSearch : function(){
 		return function(event) {
 			SearchBar.find($(this).val());
 		};
 	}
+
 
 }
